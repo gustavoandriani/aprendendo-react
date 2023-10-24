c8f04f0bdb5718ed24ce55a1832cd7897b074ecd
Gustavo Andriani <gustavo.joseandriani@gmail.com>
2023-10-24 17:18:15 -0300

    Exercicio final em andamento

--- exercicio-final/src/components/ContainerInfos/index.jsx
+++ exercicio-final/src/components/ContainerInfos/index.jsx
@@ -1,16 +1,67 @@
-import BoxInfos from "../BoxInfos"
+import { useEffect, useState } from "react"
 import "./styles.css"
-import CountDiversidade from "../../functions/CountDiversidade"
-import CountInventarioTotal from "../../functions/CountInventarioTotal"
-import CountItensAcabando from "../../functions/CountItensAcabando"
+import listaItens from "../../database.json"
+import BoxInfos from "../BoxInfos"
 
 export default function ContainerInfos() {
+    const [ listItens, setListItens ] = useState([])
+    const [ nameItem, setNameItem ] = useState("")
+    const [ qntdItem, setQntdItem ] = useState(0)
+    const [ totalItens, setTotalItens ] = useState(0)
+    const [ itensAcabando, setItensAcabando ] = useState(0)
+
+    useEffect(() => {
+        let total = 0
+        listItens.forEach(item => {
+            total += item.qntdItem
+        })
+        
+        setTotalItens(total)
+        setItensAcabando(itensAcabando)
+    }, [listItens])
+
+    const addItem = (ev) => {
+        ev.preventDefault()
+
+        const newItem = {
+            id: Math.floor(Math.random() * 100000),
+            nameItem: nameItem,
+            qntdItem: qntdItem,
+            addData: new Date()
+        }
+
+        setListItens((state) => [...state, newItem])
+        setNameItem("")
+        setQntdItem("")
+    }
+
+    console.log(listItens)
+
     return (
-        <div className="ContainerInfos">
-            <BoxInfos infoTitulo={"Diversidade de itens"} infoQntd={CountDiversidade()} />
-            <BoxInfos infoTitulo={"Inventário total"} infoQntd={CountInventarioTotal()} />
-            <BoxInfos infoTitulo={"Itens recentes"} infoQntd={0} />
-            <BoxInfos infoTitulo={"Itens acabando"} infoQntd={CountItensAcabando()} />
-        </div>
+        <>
+            <div className="ContainerInfos">
+                <BoxInfos infoTitulo={"Diversidade de itens"} infoQntd={listItens.length} />
+                <BoxInfos infoTitulo={"Inventário total"} infoQntd={totalItens} />
+                <BoxInfos infoTitulo={"Itens recentes"} infoQntd={0} />
+                <BoxInfos infoTitulo={"Itens acabando"} infoQntd={itensAcabando} />
+            </div>
+            <div>
+                <form onSubmit={addItem}>
+                    <input
+                        type="text"
+                        value={nameItem}
+                        required
+                        onChange={(ev) => setNameItem(ev.target.value)}
+                    />
+                    <input
+                        type="number"
+                        value={qntdItem}
+                        required
+                        onChange={(ev) => setQntdItem(ev.target.value)}
+                    />
+                    <button type="submit">ENVIAR</button>
+                </form>
+            </div>
+        </>
     )
 }
--- exercicio-final/src/pages/Dashboard.jsx
+++ exercicio-final/src/pages/Dashboard.jsx
@@ -2,9 +2,9 @@ import ContainerInfos from "../components/ContainerInfos";
 
 export default function Dashboard() {
     return (
-        <>
+        <main>
             <h2>Dashboard</h2>
             <ContainerInfos />
-        </>
+        </main>
     )
 }
